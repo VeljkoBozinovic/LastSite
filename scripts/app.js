8aad1c2aa91e538ca89aeb18b2f57ea54f7952bf
VeljkoBozinovic <veljkobozinovic@gmail.com>
2023-03-06 11:27:18 +0100

    final

--- scripts/app.js
+++ scripts/app.js
@@ -45,59 +45,59 @@ const elementFormSectionBall = document.querySelector(".form__wrapper__ball");
 const elementFormSectionPipe2 = document.querySelector(".form__wrapper__pipe2");
 
 const observerBusiness = new IntersectionObserver((element) => {
-  elementBusinessSectionTitle.classList.toggle("animate-business-title", element.isIntersecting);
+  elementBusinessSectionTitle.classList.toggle("animate-business-title", element[0].isIntersecting);
   elementBusinessSectionImages.forEach((image) => {
-    image.classList.toggle("animate-business-images", element.isIntersecting);
+    image.classList.toggle("animate-business-images", element[0].isIntersecting);
   });
   elementBusinessSectionTexts.forEach((text) => {
-    text.classList.toggle("animate-business-texts", element.isIntersecting);
+    text.classList.toggle("animate-business-texts", element[0].isIntersecting);
   });
 });
 observerBusiness.observe(elementBusinessSection);
 
 const observerWoman = new IntersectionObserver((element) => {
-  elementWomanSectionCircles.classList.toggle("animate-woman-circles", element.isIntersecting);
-  elementWomanSectionImage.classList.toggle("animate-woman-image", element.isIntersecting);
-  elementWomanSectionBg.classList.toggle("animate-woman-bg", element.isIntersecting);
+  elementWomanSectionCircles.classList.toggle("animate-woman-circles", element[0].isIntersecting);
+  elementWomanSectionImage.classList.toggle("animate-woman-image", element[0].isIntersecting);
+  elementWomanSectionBg.classList.toggle("animate-woman-bg", element[0].isIntersecting);
   elementWomanSectionSimpleCards.forEach((sCard) => {
-    sCard.classList.toggle("animate-woman-s-cards", element.isIntersecting);
+    sCard.classList.toggle("animate-woman-s-cards", element[0].isIntersecting);
   });
   elementWomanSectionCards.forEach((card) => {
-    card.classList.toggle("animate-woman-cards", element.isIntersecting);
+    card.classList.toggle("animate-woman-cards", element[0].isIntersecting);
   });
 });
 observerWoman.observe(elementWomanSection);
 
 const observerDigital = new IntersectionObserver((element) => {
-  elementDigitalSectionBall.classList.toggle("animate-digital-ball", element.isIntersecting);
-  elementDigitalSectionWave.classList.toggle("animate-digital-wave", element.isIntersecting);
-  elementDigitalSectionCube.classList.toggle("animate-digital-cube", element.isIntersecting);
-  elementDigitalSectionCard.classList.toggle("animate-digital-card", element.isIntersecting);
-  elementDigitalSectionWheel.classList.toggle("animate-digital-wheel", element.isIntersecting);
-  elementDigitalSectionPipe.classList.toggle("animate-digital-pipe", element.isIntersecting);
+  elementDigitalSectionBall.classList.toggle("animate-digital-ball", element[0].isIntersecting);
+  elementDigitalSectionWave.classList.toggle("animate-digital-wave", element[0].isIntersecting);
+  elementDigitalSectionCube.classList.toggle("animate-digital-cube", element[0].isIntersecting);
+  elementDigitalSectionCard.classList.toggle("animate-digital-card", element[0].isIntersecting);
+  elementDigitalSectionWheel.classList.toggle("animate-digital-wheel", element[0].isIntersecting);
+  elementDigitalSectionPipe.classList.toggle("animate-digital-pipe", element[0].isIntersecting);
 });
 observerDigital.observe(elementDigitalSection);
 
 const observerWhatWeDo = new IntersectionObserver((element) => {
-  elementWhatWeDoSectionBall.classList.toggle("animate-whatWeDo-ball", element.isIntersecting);
-  elementWhatWeDoSectionCard.classList.toggle("animate-whatWeDo-card", element.isIntersecting);
-  elementWhatWeDoSectionCube.classList.toggle("animate-whatWeDo-cube", element.isIntersecting);
-  elementWhatWeDoSectionWave.classList.toggle("animate-whatWeDo-wave", element.isIntersecting);
+  elementWhatWeDoSectionBall.classList.toggle("animate-whatWeDo-ball", element[0].isIntersecting);
+  elementWhatWeDoSectionCard.classList.toggle("animate-whatWeDo-card", element[0].isIntersecting);
+  elementWhatWeDoSectionCube.classList.toggle("animate-whatWeDo-cube", element[0].isIntersecting);
+  elementWhatWeDoSectionWave.classList.toggle("animate-whatWeDo-wave", element[0].isIntersecting);
 });
 observerWhatWeDo.observe(elementWhatWeDoSection);
 
 const observerTeam = new IntersectionObserver((element) => {
-  elementTeamSectionTitle.classList.toggle("animate-team-title", element.isIntersecting);
-  elementTeamSectionText.classList.toggle("animate-team-info", element.isIntersecting);
+  elementTeamSectionTitle.classList.toggle("animate-team-title", element[0].isIntersecting);
+  elementTeamSectionText.classList.toggle("animate-team-info", element[0].isIntersecting);
 });
 observerTeam.observe(elementTeamSectionContainer);
 
 const observerForm = new IntersectionObserver((element) => {
-  elementFormSectionBall.classList.toggle("animate-form-ball", element.isIntersecting);
-  elementFormSectionPipe.classList.toggle("animate-form-pipe", element.isIntersecting);
-  elementFormSectionPipe2.classList.toggle("animate-form-pipe2", element.isIntersecting);
-  elementFormSectionPottle.classList.toggle("animate-form-pottle", element.isIntersecting);
-  elementFormSectionWave.classList.toggle("animate-form-wave", element.isIntersecting);
-  elementFormSectionWheel.classList.toggle("animate-form-wheel", element.isIntersecting);
+  elementFormSectionBall.classList.toggle("animate-form-ball", element[0].isIntersecting);
+  elementFormSectionPipe.classList.toggle("animate-form-pipe", element[0].isIntersecting);
+  elementFormSectionPipe2.classList.toggle("animate-form-pipe2", element[0].isIntersecting);
+  elementFormSectionPottle.classList.toggle("animate-form-pottle", element[0].isIntersecting);
+  elementFormSectionWave.classList.toggle("animate-form-wave", element[0].isIntersecting);
+  elementFormSectionWheel.classList.toggle("animate-form-wheel", element[0].isIntersecting);
 });
 observerForm.observe(elementFormSection);
